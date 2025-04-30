2779a89423b5200c6c964d25544da48d5a8085af
Itay Atun <itay14717@gmail.com>
2025-04-30 17:45:21 +0300

    Player and hitbox

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pygame
 
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        clock.tick(60) / 1000
+        # dt = clock.tick(60) / 1000
+
+
+def fun_main():
+    return True
 
 
 if __name__ == "__main__":
